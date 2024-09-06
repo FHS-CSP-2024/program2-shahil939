093bf5e728ebbed9158b7a8f6344688d21dca975
shahil939 <shahilray15@gmail.com>
2024-09-06 14:54:06 +0000

    finished

--- Program2.py
+++ Program2.py
@@ -56,7 +56,7 @@ print(name)
 # - Prints it out twice on a single line so that there is an exclamation mark at the beginning of the line, 
 # - another between the two names and a third one at the end of the line.
 name = input("What is your name? ")
-name ("!"+name+"!"+name+"!")
+print("!" + name + "!" + name + "!")
 
 ## Problem 3 ##
 #Please write a script that: 
@@ -67,23 +67,22 @@ name ("!"+name+"!"+name+"!")
 #   - Last name: Sanders
 #   - Street address: 91 Station Road
 #   - City and postal code: Folsom CA, 95630
-firstname = input("Tell me your name first name ")
-lastname = input("Tell me your last name ")
-adress = input("what is your street adress: ")
+first=input("Tell me your name first name: ")
+last=input("Tell me your last name: ")
+street_adress=input("what is your street adress: ")
 city=input("what us your city and postal code: ")
-print("first name: "+firstname)
-print("last name:"+lastname)
-print("street adress: "+ adress)
+print("First Name: "+first)
+print("Last Name:"+last)
+print("street adress: "+ street_adress)
 print("city and postal code: "+city)
 ## Problem 4 ##
 #Please write a script that: 
 # - Asks for 3 words 
 # - Puts the words together with dashes and prints that out
 one= input("Give me one word: ")
-two= input("give me another word ")
-three= input("Give me the last word")
-print( one+"-"two+"+three)
-
+two= input("give me another word: ")
+three= input("Give me the last word: ")
+print(one + "-" + two + "-" + three)
 ## Problem 5 ##
 #Please write a script that: 
 # - Asks for a name and a year
@@ -95,3 +94,7 @@ print( one+"-"two+"+three)
 #Mary is a valiant knight, born in the year 1572. 
 #One morning Mary woke up to an awful racket: a dragon was approaching the village. 
 #Only Mary could save the village's residents.
+name=input("Tyoe in a name: ")
+year=input("Tpye in a year: ")
+print("The year was " + year + " and there was a dangerous dog named " + name + " outside of the house.")
+print(name + " The dog was still outside of my house for a long time, after an 1 hour the pet control came.")
